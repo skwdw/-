29d093d745e10a05a1260390ff1e1136f92311f6
skwdw <75801773+skwdw@users.noreply.github.com>
2022-04-06 18:44:16 +0900

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,35 @@
+Config = {}
+
+Config.Playlist = {
+   [0] = {name = "stupid_question" , skip = "67"},
+   [1] = {name = "perhaps_love" , skip = "59"},
+   [2] = {name = "loving_you" , skip = "82"},
+   [3] = {name = "wip_wup" , skip = "13"},
+}
+Config.Dances = {
+   [0] = {dict = "anim@amb@nightclub@mini@dance@dance_solo@male@var_b@",anim = "low_center"}, 
+   [1] = {dict = "anim@amb@nightclub@dancers@solomun_entourage@",anim = "mi_dance_facedj_17_v1_female^1"},
+   [2] = {dict = "anim@amb@nightclub@mini@dance@dance_solo@female@var_a@",anim =  "high_center"},
+   [3] = {dict = "anim@amb@nightclub@mini@dance@dance_solo@female@var_a@",anim =  "high_center_up"},
+   [4] = {dict = "anim@amb@nightclub@dancers@crowddance_facedj@hi_intensity",anim =  "hi_dance_facedj_09_v2_female^1"},
+   [5] = {dict = "anim@amb@nightclub@dancers@crowddance_facedj@hi_intensity",anim =  "hi_dance_facedj_09_v2_female^3"},
+   [6] = {dict = "anim@amb@nightclub@mini@dance@dance_solo@female@var_a@",anim =  "high_center_up"},
+   [7] = {dict = "anim@amb@nightclub@dancers@podium_dancers@",anim =  "hi_dance_facedj_17_v2_male^5"},
+   [8] = {dict = "anim@amb@nightclub@mini@dance@dance_solo@male@var_b@",anim =  "high_center_down"},
+   [9] = {dict = "anim@amb@nightclub@mini@dance@dance_solo@male@var_a@",anim =  "high_center"},
+   [10] = {dict = "anim@amb@nightclub@mini@dance@dance_solo@male@var_b@",anim =  "high_center_up"},
+   [11] = {dict = "rcmnigel1bnmt_1b",anim =  "dance_loop_tyler"},
+   [12] = {dict = "misschinese2_crystalmazemcs1_cs",anim =  "dance_loop_tao"},
+   [13] = {dict = "misschinese2_crystalmazemcs1_ig",anim =  "dance_loop_tao"},
+   [14] = {dict = "missfbi3_sniping",anim =  "dance_m_default"},
+   [15] = {dict = "special_ped@mountain_dancer@monologue_3@monologue_3a",anim =  "mnt_dnc_buttwag"},
+   [16] = {dict = "move_clown@p_m_zero_idles@",anim =  "fidget_short_dance"},
+   [17] = {dict = "move_clown@p_m_two_idles@",anim =  "fidget_short_dance"},
+   [18] = {dict = "anim@amb@nightclub@lazlow@hi_podium@", anim = "danceidle_hi_11_buttwiggle_b_laz"},
+   [19] = {dict = "timetable@tracy@ig_5@idle_a",anim =  "idle_a"},
+   [20] = {dict = "timetable@tracy@ig_8@idle_b",anim =  "idle_d"},
+   [21] = {dict = "anim@amb@nightclub@mini@dance@dance_solo@female@var_a@",anim = "med_center_up"},
+   [22] = {dict = "anim@mp_player_intcelebrationfemale@the_woogie", anim = "the_woogie"},
+   [23] = {dict = "anim@amb@casino@mini@dance@dance_solo@female@var_b@", anim = "high_center"},
+   [24] = {dict = "anim@amb@casino@mini@dance@dance_solo@female@var_a@",anim =  "med_center"},
+}
